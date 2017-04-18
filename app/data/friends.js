39f1d73d35a6daeec7d1ad09b410ddb1fe21488b
Yousrat <ybanna@gmail.com>
2017-04-18 13:23:03 -0700

    first instance of app

---
+++ app/data/friends.js
@@ -0,0 +1,59 @@
+// ===============================================================================
+// USER DATA
+//===============================================================================
+
+var userArray = [
+  {
+    name: "Ahmed",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+    
+  },{
+    name: "Mandy",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/34312345.jpg",
+    scores: [
+      4,
+      2,
+      4,
+      4,
+      5,
+      2,
+      1,
+      3,
+      2,
+      1
+    ]
+    
+  },{
+    name: "Sandy",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/123r5.jpg",
+    scores: [
+      1,
+      2,
+      3,
+      4,
+      5,
+      1,
+      2,
+      3,
+      4,
+      5
+    ]
+    
+  }
+];
+
+
+// make data available to other files in the project
+module.exports = userArray;
